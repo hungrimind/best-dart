1327dd9bc3eb238674437b0d1cdd72e1afc6823e
Tadas Petra <tadas@tadaspetra.com>
2025-10-21 12:53:41 -0500

    read user inputs

---
+++ favorite_city/solution/bin/favorite_city.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  print('What is your favorite city?');
+
+  // 1. Read the user's input from the console.
+  // 2. Store the input in a variable named 'city'.
+  String? city = stdin.readLineSync();
+
+  print('Wow, $city sounds like a great place to visit!');
+}
