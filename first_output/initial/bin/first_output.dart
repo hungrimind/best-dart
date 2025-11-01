d172da4797aa9fd8b846c64aa2735d3478fe566d
Tadas Petra <tadas@tadaspetra.com>
2025-11-01 10:57:29 -0500

    Remove code comments

--- first_output/initial/bin/first_output.dart
+++ first_output/initial/bin/first_output.dart
@@ -1,7 +1,9 @@
 void main() {
   // TODO: Use print statements to debug and see what's happening!
-  int three = 2; // This variable should hold the value 3
+  int three = 2;
+
   int result = 5 + three;
+
   print('The result is: $result');
   // Expected output: "The result is: 8"
   // But we're getting the wrong answer - use print to debug!
--- first_output/solution/bin/first_output.dart
+++ first_output/solution/bin/first_output.dart
@@ -1,8 +1,6 @@
 void main() {
-  // Debugging with print statements:
-  int three = 3; // Fixed: Changed from 2 to 3
-  print(
-      'Debugging: The variable three is set to $three'); // Print to see what value three actually has
+  int three = 3;
+  print('Debugging: The variable "three" is set to $three');
 
   int result = 5 + three;
 
