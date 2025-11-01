3d2473a0dd5bd915cf06601fb18ef18663cbe935
Tadas Petra <tadas@tadaspetra.com>
2025-11-01 18:35:01 -0500

    Make the initial lesson better

--- variable_practice/initial/bin/variable_practice.dart
+++ variable_practice/initial/bin/variable_practice.dart
@@ -1,7 +1,8 @@
 void main() {
-  // 1. Declare a variable named 'currentStatus' with the value "Feeling Great".
-  // TODO: Declare the variable here
+// 1. Declare a variable named 'currentStatus' with the value "Feeling Great".
+  var currentStatus;
+  print("Current status: $currentStatus");
 
-  // 2. On the next line, update its value to "Ready to Code".
-  // TODO: Update the variable here
+// 2. On the next line, update its value to "Ready to Code".
+  print("Current status: $currentStatus");
 }
