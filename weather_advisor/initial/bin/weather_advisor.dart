18334a0d255ff5fd9f4b491e13964b80143b66cd
Tadas Petra <tadas@tadaspetra.com>
2025-11-09 10:03:03 -0600

    update the starting code

--- weather_advisor/initial/bin/weather_advisor.dart
+++ weather_advisor/initial/bin/weather_advisor.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 void main() {
+  print("Enter the temperature: ");
   int temperature = int.parse(stdin.readLineSync()!);
 
   // TODO: Write an if statement that checks if temperature is greater than 25
--- weather_advisor/solution/bin/weather_advisor.dart
+++ weather_advisor/solution/bin/weather_advisor.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
 void main() {
+  print("Enter the temperature: ");
   int temperature = int.parse(stdin.readLineSync()!);
-  
+
   if (temperature > 25) {
     print("It's a hot day! Drink plenty of water.");
   }
 }
-
