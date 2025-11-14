d426ff9de402c08cd09ac8ad2f813a7834129da2
Tadas Petra <tadas@tadaspetra.com>
2025-11-14 12:40:06 -0600

    remove unneccessary hints

--- leaderboard_update/initial/bin/leaderboard_update.dart
+++ leaderboard_update/initial/bin/leaderboard_update.dart
@@ -3,12 +3,8 @@ void main() {
   List<int> scores = [1200, 1150, 1020, 950, 880];
 
   // TODO: Task 1 - Print the top score (first element in the list)
-  // Hint: Access the first element using index 0
 
   // TODO: Task 2 - Update the lowest score (last element) to 915
-  // Hint: Use scores.length - 1 to get the index of the last element
 
   // TODO: Task 3 - Print the entire updated leaderboard
-  // Hint: Simply print the scores list
 }
-
