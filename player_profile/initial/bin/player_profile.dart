018cc27d3fdb92eeb643f02a8f0ab70f74d0aa4d
Tadas Petra <tadas@tadaspetra.com>
2025-12-05 13:15:17 -0600

    player-profile-class

---
+++ player_profile/initial/bin/player_profile.dart
@@ -0,0 +1,30 @@
+class Player {
+  // TODO: Define properties with default values
+  // Add a String property named 'username' with default value 'Guest'
+  // Add an int property named 'health' with default value 100
+  // Add a double property named 'score' with default value 0.0
+}
+
+void main() {
+  // Create two player instances
+  Player player1 = Player();
+  Player player2 = Player();
+  
+  // Modify player1's properties
+  player1.username = 'Alex';
+  player1.health = 85;
+  player1.score = 1500.5;
+  
+  // Print player1's information
+  print('Player 1:');
+  print('Username: ${player1.username}');
+  print('Health: ${player1.health}');
+  print('Score: ${player1.score}');
+  
+  // Print player2's information
+  print('\nPlayer 2:');
+  print('Username: ${player2.username}');
+  print('Health: ${player2.health}');
+  print('Score: ${player2.score}');
+}
+
