90487abe301dae5a1d9361fb21d685dc5f7b0583
Tadas Petra <tadas@tadaspetra.com>
2025-12-12 09:40:49 -0600

    add a message that says to wait 2 seconds

--- fetching_welcome_message/initial/bin/fetch_welcome_message.dart
+++ fetching_welcome_message/initial/bin/fetch_welcome_message.dart
@@ -1,6 +1,7 @@
 // This function simulates fetching data from a server.
 // After a 2-second delay, it completes with the welcome message.
 Future<String> fetchWelcomeMessage() async {
+  print("Wait 2 seconds...");
   await Future.delayed(Duration(seconds: 2));
   return "Welcome to our app!";
 }
